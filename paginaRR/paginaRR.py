0ac0352fb4c7a555d29f5223d77b1ee9b5fddb15
JuanQ-04 <jquinone11@cuc.edu.co>
2025-04-25 11:23:38 -0500

    Actualizacion del codigo python para la pagina web, ademas de agregar 3 imagenes

--- paginaRR/paginaRR.py
+++ paginaRR/paginaRR.py
@@ -16,24 +16,129 @@ def index() -> rx.Component:
     return rx.container(
         rx.color_mode.button(position="top-right"),
         rx.vstack(
-            rx.heading("full gass to Reflex!", size="9"),
+            rx.divider(),
+            rx.heading("¿Qué es la ciberseguridad?", size="9"),
+
+            rx.flex(
+                rx.center(rx.text(
+                "La ciberseguridad, también conocida como seguridad digital, es la práctica de proteger su información digital, dispositivos y activos. Esto incluye información personal, cuentas, archivos, fotos e incluso el dinero.",
+                size="5",
+                ),),
+                rx.spacer(),
+                rx.center(rx.image(src="ciberseguridad.jpg", width="100%", height="100%"),),
+                width="100%",
+            ),
+            rx.divider(),
+            rx.heading("CIA", size="7"),
             rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
+                "El acrónimo 'CIA' se usa a menudo para representar los tres pilares de la ciberseguridad.",
                 size="5",
             ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
+            rx.flex(
+                rx.hstack(
+                    rx.icon("file-lock", stroke_width=2.5),
+                    rx.text(
+                        rx.text.strong("Confidencialidad: "),
+                        "Mantener los secretos y garantizar que solo los usuarios autorizados puedan obtener acceso a sus archivos y cuentas.",
+                        as_="p"
+                    ),
+                    spacing="2"
+                ),
+                rx.hstack(
+                    rx.icon("shield-check", stroke_width=2.5),
+                    rx.text(
+                        rx.text.strong("Integridad: "),
+                        "Garantizar que su información es la que debe ser y que nadie ha insertado, modificado o eliminado cosas sin su permiso.",
+                        as_="p"
+                    ),
+                    spacing="2"
+                ),
+                rx.hstack(
+                    rx.icon("folder-key", stroke_width=2.5),
+                    rx.text(
+                        rx.text.strong("Acceso: "),
+                        "Garantizar que puede tener acceso a su información y sistemas cuando lo necesite.",
+                        as_="p"
+                    ),
+                    spacing="2"
+                ),
+                direction="column",
+                spacing="3"
+            ),
+            rx.divider(),
+            rx.heading("La seguridad es un proceso, no un producto", size="7"),
+            rx.text(
+                "Aunque las aplicaciones de seguridad y los dispositivos, como el software antimalware y los firewalls, son esenciales, no basta con conectar esas herramientas y darlo por hecho. La seguridad digital también requiere que se establezca un conjunto de procesos y procedimientos adecuados. Son, entre otros:",
+                size="5",
+            ),
+            rx.flex(
+                rx.center(
+                    rx.list.unordered(
+                        rx.list.item("Copias de seguridad de datos"),
+                        rx.list.item("Buenos hábitos cibernéticos"),
+                        rx.list.item("Mantenga su software actualizado"),
+                        rx.list.item("Usar contraseñas seguras y únicas"),
+                        rx.list.item("Usar la autenticación multifactor"),
+                        rx.list.item("Bloquear los dispositivos"),
+                    ),
+                ),
+                rx.spacer(),
+                rx.center(
+                    rx.image(src="imagen2.png", width="200px", height="200px"),
+                ),
+                width="100%",
+                direction="row",
+                align="center",
+                justify="between",
+            ),
+            rx.divider(),
+            rx.heading("Vea también", size="5"),
+            rx.text(
+                "Entérate de las noticias más recientes ",
+                rx.link(
+                    rx.button("Aquí"),
+                    href="https://www.youtube.com/@AsperisSecurity",
+                    is_external=True,
+                ),
+            ),
+            rx.video(
+                url="https://youtu.be/TM-OT1U3P0k?si=lApepEd66mT3vsBr",
+                width="50%",
+                height="300px",
+            ),
+                        rx.text(
+                "Introduccion a la ciberseguridad ",
+                rx.link(
+                    rx.button("Aquí"),
+                    href="https://www.educaopen.com/digital-lab/blog/guias-digitales/introduccion-a-la-ciberseguridad",
+                    is_external=True,
+                ),
+            ),
+
+            rx.divider(),
+
+            rx.image(
+                src="logo.png",
+                width="100px",
+                height="auto",
+                border_radius="15px 50px",
+                border="5px solid #555",
             ),
             spacing="5",
             justify="center",
             min_height="85vh",
         ),
-        rx.logo(),
+
+        
     )
 
 
-app = rx.App()
+app = rx.App(
+    theme=rx.theme(
+        has_background=True,
+        appearance="dark",
+        radius="large",
+        accent_color="ruby",
+    )
+)
 app.add_page(index)
